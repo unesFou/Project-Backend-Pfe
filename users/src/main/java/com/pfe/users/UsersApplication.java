245eecf83df03e88ebdfe9f8b586eabb055955de
unesFou <foughaliyouness@gmail.com>
2025-01-02 17:26:46 +0100

    Euruka et Getway

--- users/src/main/java/com/pfe/users/UsersApplication.java
+++ users/src/main/java/com/pfe/users/UsersApplication.java
@@ -2,8 +2,9 @@ package com.pfe.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UsersApplication {
 
     public static void main(String[] args) {
--- vehicle/src/main/java/pfe/vehicle/vehicle/repository/VehicleRepository.java
+++ vehicle/src/main/java/pfe/vehicle/vehicle/repository/VehicleRepository.java
@@ -1,6 +1,5 @@
 package pfe.vehicle.vehicle.repository;
 
-
 import pfe.vehicle.vehicle.model.Vehicle;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
